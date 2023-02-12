9c9c6964df712c24550c6bc61b0d374b855e939d
MUHAMAD ASHRAFF <65198559+iamashraff@users.noreply.github.com>
2023-02-12 13:50:05 +0800

    Commit

---
+++ src/ZOOApp20509.java
@@ -0,0 +1,51 @@
+public class ZOOApp20509 {
+
+    private int statusResidence; //Declare instance variable as private
+    /*The variable statusResidence outside of method
+    so that every method can access the variable value.*/
+
+    public ZOOApp20509(int s) //constructor with parameter
+    {
+        statusResidence = s; //Set value of statusResidence
+    }//end of constructor
+
+    //Non-void method with parameter
+    public int GetPrice(int adultQty, int childQty, int seniorQty) //Header Method //3 integer parameter received
+    {
+        //Declare variable
+        int price[] = new int[3];//Declare array price[]: size of 3
+        int Quantity[] = {adultQty,childQty,seniorQty};//Declare array Quantity[] and set the value.
+        int totalFees=0;
+
+    //Use switch case to set the price of each category//
+    switch(statusResidence) {
+    case 1 :
+        price[0] = 44; //Adult Price
+        price[1] = 16; //Child Price
+        price[2] = 21; //Senior Price
+        break;
+    case 2 :
+        price[0] = 63; //Adult Price
+        price[1] = 33; //Child Price
+        price[2] = 63; //Senior Price
+        break;
+    case 3 :
+        price[0] = 85; //Adult Price
+        price[1] = 43; //Child Price
+        price[2] = 85; //Senior Price
+        break;
+    default:
+    //INPUT IS INVALID
+    }//END SWITCH CASE
+
+        //Process of calculating the grand total of the price
+        for (int i=0; i<price.length; i++)
+        {
+            price[i] = price[i] * Quantity[i];
+            totalFees +=price[i];
+        }
+        return totalFees; //The return value
+    }//end of method GetPrice
+
+}//end of java class
+
---
+++ src/ZOOApp20509Test.java
@@ -0,0 +1,110 @@
+/*
+-------------------------CODED BY-----------------------------
+MUHAMAD ASHRAFF BIN OTHMAN 		                  52213120560
+MUHAMMAD HADIF BIN ABDUL RAHIM 		              52213120450 
+SITI NABILAH BINTI MOHD HANAFI 		              52213120509 
+SHARIFAH NURUL FATIHAH BINTI WAN ASHHAR MARZUKI   52213120482
+---------------------------FOR--------------------------------
+                    MADAM SURIANA ISMAIL
+--------------------------------------------------------------                    
+ */
+import java.util.*;
+public class ZOOApp20509Test {
+
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    //Declare variable
+    int sentinelLoop;
+    int adultQty, childQty, seniorQty;
+    int statusResidence;
+
+    //Initialize Sentinel Loop
+    System.out.print("Press any key to start or '0' to stop > ");
+    sentinelLoop = scan.nextInt();
+
+    //Sentinel DOWHILE
+        while(sentinelLoop!=0)
+        {
+        System.out.println("======================================================");
+        System.out.println("                   ZOO NEGARA MALAYSIA                ");
+        System.out.println("                Hulu Kelang, 68000 Ampang             ");
+        System.out.println("                   Selangor Darul Ehsan               ");
+        System.out.println("------------------------------------------------------");
+        System.out.println("              Ticket Price Calculator                 ");
+        System.out.println("Entrance Fee                                          ");
+        System.out.println("---------------                                       ");
+        System.out.println("------------------------------------------------------");
+        System.out.println("|         |  MALAYSIAN   |         FOREIGNER         |");
+        System.out.println("|         |              |    ASEAN   |    OTHERS    |");
+        System.out.println("------------------------------------------------------");
+        System.out.println("|ADULT    |    RM 44     |    RM63    |     RM85     |");
+        System.out.println("|CHILDREN |    RM 16     |    RM33    |     RM43     |");
+        System.out.println("|SENIOR   |    RM 21     |    RM63    |     RM85     |");
+        System.out.println("------------------------------------------------------");
+        System.out.println("\n");
+
+        //Prompt and Get statusResidence
+        System.out.println("Select Residence Status :");
+        System.out.println("-------------------------");
+        System.out.println("1 - Malaysian");
+        System.out.println("2 - Asean");
+        System.out.println("3 - Others");
+        System.out.print("  Enter your selection : ");
+        statusResidence = scan.nextInt();
+        System.out.println("-------------------------");
+        
+
+         //Use DOWHILE loop structure
+            /* Using DOWHILE to make sure that the user
+            input for statusResidence with range value
+            1 to 3 only.*/
+        while (statusResidence>3 || statusResidence<1)
+        {
+            System.out.println("\n");
+            System.out.println("Your input is invalid. Please try again !");
+            System.out.println("-------------------------");
+            System.out.println("\n");
+        System.out.println("Select Residence Status :");
+        System.out.println("-------------------------");
+        System.out.println("1 - Malaysian");
+        System.out.println("2 - Asean");
+        System.out.println("3 - Others");
+        System.out.print("  Enter your selection : ");
+        statusResidence = scan.nextInt();
+        System.out.println("-------------------------");
+        }
+    
+        //Declare object to call zooProcess class
+        ZOOApp20509 myZooProcess = new ZOOApp20509(statusResidence);
+
+        //Prompt and Get Ticket Quantity
+        System.out.println("\n");
+        System.out.println("-----------------------");
+        System.out.println("Enter Ticket Quantity :");
+        System.out.println("-----------------------");
+        System.out.print("Adult  > ");
+        adultQty = scan.nextInt();
+        System.out.print("Child  > ");
+        childQty = scan.nextInt();
+        System.out.print("Senior > ");
+        seniorQty = scan.nextInt();
+        System.out.println("-----------------------");
+
+        //Declaring variable to get return values of method GetPrice
+        int totalFees = myZooProcess.GetPrice(adultQty, childQty, seniorQty);
+
+        //Output
+        System.out.println("\n");
+        System.out.println("THANK YOU FOR YOUR PURCHASE !");
+        System.out.println("Total Fees : RM" + totalFees);
+
+        //Update Sentinel Loop
+        System.out.print("Press any key to continue or '0' to stop > ");
+        sentinelLoop = scan.nextInt();
+        }//END DO
+        
+        System.out.println("Thank you !");
+    }//END MAIN
+}//END CLASS
+
